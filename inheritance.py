8fc588f415b4edb18c0a5b3c6117efec964b53c8
shiwammittal <shiwammittal@gmail.com>
2017-03-24 23:47:34 -0400

    ClassParent
    demonstrate how a sub class inherits the properties of its parent class.

---
+++ inheritance.py
@@ -0,0 +1,7 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+shiwam_mittal = Parent("mittal","black")
+print(shiwam_mittal.last_name)
